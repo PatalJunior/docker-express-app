77e2a6ff73fc3b1c261cff09ffbc82783141d332
Eduardo Abrantes <81658559+PatalJunior@users.noreply.github.com>
2024-10-23 16:24:14 +0100

    Update webpage

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ const express = require('express');
 const app = express();
 const redis = require('redis');
 const redisClient = redis.createClient({
-  host: 'redis',
-  port: 6379
+    host: 'redis',
+    port: 6379
 });
 
-app.get('/', function(req, res) {
-    redisClient.get('numVisits', function(err, numVisits) {
+app.get('/', function (req, res) {
+    redisClient.get('numVisits', function (err, numVisits) {
         numVisitsToDisplay = parseInt(numVisits) + 1;
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
         }
-       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
+        res.send(`<span style="font-size: 4em;">${os.hostname().charAt(0).toUpperCase()}
+       <h1>Number of visits is: ${numVisitsToDisplay}</h1>
+       </span>
+       `)
         numVisits++;
         redisClient.set('numVisits', numVisits);
     });
 });
 
-app.listen(5000, function() {
+app.listen(5000, function () {
     console.log('Web application is listening on port 5000');
 });
